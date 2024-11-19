b2773b9318a0a26b70a209aa6d3d0e1d3174c7cb
subhana Thasni <subhanathasni@gmail.com>
2024-11-19 18:58:53 +0530

    google sign up

--- config/passport_setup.js
+++ config/passport_setup.js
@@ -7,7 +7,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL:process.env.GOOGLE_REDIRECT_URI,
     scope: ['profile', 'email']
 },
 async function(accessToken, refreshToken, profile, done) {
