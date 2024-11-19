b343068e8e90be5c533817c8b48b006342a5116a
subhana Thasni <subhanathasni@gmail.com>
2024-11-19 10:38:42 +0530

    app.js

--- app.js
+++ app.js
@@ -51,12 +51,10 @@ app.use((req, res, next) => {
 });
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(routes)
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
